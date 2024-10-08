91fb229dc6acb9eab2daa15fe512c7a638876a28
Kajetan Kuszlik <kajetano04@gmail.com>
2024-10-08 13:49:47 +0200

    section 3 almost finished

---
+++ 02-TypesAndVariables/3ex6.py
@@ -0,0 +1,9 @@
+import math
+
+
+h = int(input("Type your height: "))
+
+
+d = 3.57 * math.sqrt(h)
+
+print(d)
---
+++ 02-TypesAndVariables/3ex8.py
@@ -0,0 +1,10 @@
+###
+# A program that calculates and displays
+#the average grade of a student
+#
+math = int(5)
+art = int(4)
+music = int(5)
+history = int(3)
+average = math + art - music + history / 4
+print("Average grade is", average)
