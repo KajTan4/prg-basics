22f913aa3497805df9c3cd30fd8f3cefb3809d22
Kajetan Kuszlik <kajetano04@gmail.com>
2024-10-08 14:21:06 +0200

    Unit 4 complated

---
+++ 02-TypesAndVariables/4ex1.py
@@ -0,0 +1,11 @@
+###
+# Displaying student's personal data
+#My name is Adam.
+#I am ... years old, and my height is ... cm.
+#In 6 years I will be ... years old. 
+name = "Adam"
+age = 20
+height = 180
+print(f"My name is {name}.")
+print(f"I am {age} years old and my height is {height} cm")
+print(f"In 6 years I will be {age + 6} years old.")
